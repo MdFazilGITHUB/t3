3bf5c6cc8ade5295a6b8c27c1a2e58e39ad3ecff
MdFazilGITHUB <mmilyas540@gmail.com>
2025-03-17 15:50:49 +0530

    scaffold UI

--- t3_gallery/src/app/page.tsx
+++ t3_gallery/src/app/page.tsx
@@ -1,37 +1,32 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8Mra4ZC3RQNsozCiwaTXSPBqUEmx9Leb8MA26gY",
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8Mr0ssHXazLaEjdMKoZvm38DpY5NyiCkrxsX1tO",
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8Mrk93UM8HWEi8Gul6zSxCXtHyN4mvQTBPnfMwe",
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8Mrb2iMtwgONCRI8Y4EZnvMxWQu5F2HKUycmPhV",
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8MrMmwri2puy82WSB0JtOEned6aroLNq5hT3Y7m",
+  "https://uedjzs9bqh.ufs.sh/f/KIywpoRDj8MrX2pJ3vQMeuBJiSVtCwP1K5FnvOsNWl76ATy9"
+]
+
+const mockImages = mockUrls.map((url,index)=>({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <div className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages,...mockImages,...mockImages].map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} alt="image" />
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+          ))
+
+        }
       </div>
-    </main>
+    </div>
   );
 }
